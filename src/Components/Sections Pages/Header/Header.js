11cdda4b5ef18f981b92f104e625bdaa0132f9ed
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-08-28 13:54:14 -0300

    responsive

--- src/Components/Sections Pages/Header/Header.js
+++ src/Components/Sections Pages/Header/Header.js
@@ -58,37 +58,3 @@ const Header = () => {
 
 export default Header;
 
-{
-  /* <nav className="navbar">
-        <div className="logo-container">
-          <img src={logo} alt="Logo" className="logo" />
-        </div>
-        <ul className="nav-menu">
-          <li className="nav-item">
-            <a href="/#productos" className="nav-link">
-              Inicio
-            </a>
-          </li>
-          <li className="nav-item disable">
-            <a href="/nosotros" className="nav-link">
-              Nosotros
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/#productos" className="nav-link">
-              Productos
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="#productos" className="nav-link">
-              Mayoristas
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="/que-pala-comprar" className="nav-link">
-              Que Pala Comprar ?
-            </a>
-          </li>
-        </ul>
-      </nav> */
-}
