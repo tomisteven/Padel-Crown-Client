725d9417116b64b041355d2a3bc13f48816518ea
Tomas Steven Desktop <tomssteven22@gmail.com>
2023-10-19 13:31:02 -0300

    personalizar

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,6 +1,6 @@
 import { createContext} from "react";
 
-const productos = await fetch("https://padelcrown-server-4u2s-dev.fl0.io/");
+const productos = await fetch("https://padelcrown-server-dev-rhda.1.us-1.fl0.io");
 const revendedores = await fetch("https://padelcrown-server-4u2s-dev.fl0.io/revendedores");
 const revendedoresData = await revendedores.json();
 const data = await productos.json();
