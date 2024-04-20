6c0e464db45645b9e932fcea635d40e72aec049f
Tomas Steven Desktop <tomssteven22@gmail.com>
2024-04-20 17:14:32 -0300

    volviendo a rifa, optimizando context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -1,32 +1,22 @@
-import { createContext} from "react";
+import { createContext } from "react";
 
 const production = true;
 
-
-//Clientes
-if(window.location.href.includes("/admin/clientes")){
-  const clientes = await fetch(production ? "https://particular-bernita-digitalcode.koyeb.app/admin/clientes" : "http://localhost:8080/admin/clientes" , {
+const productos = await fetch(
+  "https://particular-bernita-digitalcode.koyeb.app/"
+);
+const clientes = await fetch(
+  production
+    ? "https://particular-bernita-digitalcode.koyeb.app/admin/clientes"
+    : "http://localhost:8080/admin/clientes",
+  {
     headers: {
-        'Authorization': "token_padelcrown",
-        'Content-Type': 'application/json',
-    }
-})
-  var clientesData = await clientes.json();
-}
-
-
-//Productos
-if(window.location.href.includes("/admin/productos") || window.location.href === "http://localhost:3000/" || window.location.href === "https://padelcrown.store"){
-  const productos = await fetch(production ? "https://particular-bernita-digitalcode.koyeb.app" : "http://localhost:8080" , {
-    headers: {
-        'Authorization': "token_padelcrown",
-        'Content-Type': 'application/json',
-    }
-})
-  var data = await productos.json();
-}
-
-
-
+      Authorization: "token_padelcrown",
+      "Content-Type": "application/json",
+    },
+  }
+);
+const data = await productos.json();
+const clientesData = await clientes.json();
 
 export const GlobalContext = createContext([data, clientesData]);
