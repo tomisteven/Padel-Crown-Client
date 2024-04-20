44644187ec8aee07bcee4e52c968ee7a97ab60eb
Tomas Steven Desktop <tomssteven22@gmail.com>
2024-04-20 17:09:19 -0300

    volviendo a rifa, optimizando context

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -3,17 +3,29 @@ import { createContext} from "react";
 const production = true;
 
 
-const productos = await fetch("https://particular-bernita-digitalcode.koyeb.app/");
-const clientes = await fetch(production ? "https://particular-bernita-digitalcode.koyeb.app/admin/clientes" : "http://localhost:8080/admin/clientes" , {
+//Clientes
+if(window.location.href.includes("/admin/clientes")){
+  const clientes = await fetch(production ? "https://particular-bernita-digitalcode.koyeb.app/admin/clientes" : "http://localhost:8080/admin/clientes" , {
     headers: {
         'Authorization': "token_padelcrown",
         'Content-Type': 'application/json',
     }
 })
-const data = await productos.json();
-const clientesData = await clientes.json();
+  var clientesData = await clientes.json();
+}
 
-console.log(clientesData);
+
+//Productos
+if(window.location.href.includes("/admin/productos") || window.location.href === "http://localhost:3000/" || window.location.href === "https://padelcrown.store"){
+  const productos = await fetch(production ? "https://particular-bernita-digitalcode.koyeb.app" : "http://localhost:8080" , {
+    headers: {
+        'Authorization': "token_padelcrown",
+        'Content-Type': 'application/json',
+    }
+})
+  var data = await productos.json();
+}
+
 
 
 
